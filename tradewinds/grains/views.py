2a4094f16916373426b308021eee27bae52d3b64
Nikhil Garg <nikhilgarg72@gmail.com>
2015-10-05 12:28:11 +0530

    addFirm added

--- tradewinds/grains/views.py
+++ tradewinds/grains/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.views import generic
+from django.core.urlresolvers import reverse
+from .models import User, Firm
 
 # Create your views here.
+user = User.objects.get(username='nikhil')
+
+class IndexView(generic.ListView):
+    template_name = 'grains/home.html'
+    context_object_name = 'firm_list'
+
+    def get_queryset(self):
+        """Return the firm names."""
+        return Firm.objects.all().filter(owner=user)
+
+
+def addFirm(request):
+    firm_name = request.POST['firm']
+    firm = Firm(name=firm_name, owner=user)
+    firm.save()
+    return HttpResponseRedirect(reverse('grains:index'))
+
+
--- tradewinds/tradewinds/urls.py
+++ tradewinds/tradewinds/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^grains/', include('grains.urls', namespace="grains")),
     url(r'^admin/', include(admin.site.urls)),
 ]
